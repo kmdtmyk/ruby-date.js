6252ed27dfe22b45a4e861e8d527998158fca33d
kmdtmyk <winback_banderas@yahoo.co.jp>
2021-04-16 18:52:28 +0900

    Add method toString

--- src/ruby-date.test.ts
+++ src/ruby-date.test.ts
@@ -15,6 +15,15 @@ test('toDate', () => {
   expect(date.toDate()).toBeInstanceOf(Date)
 })
 
+test('toString', () => {
+  expect(new RubyDate(1, 2, 3).toString()).toEqual('0001-02-03')
+  expect(new RubyDate(10, 2, 3).toString()).toEqual('0010-02-03')
+  expect(new RubyDate(100, 2, 3).toString()).toEqual('0100-02-03')
+  expect(new RubyDate(1000, 2, 3).toString()).toEqual('1000-02-03')
+  expect(new RubyDate(10000, 2, 3).toString()).toEqual('10000-02-03')
+  expect(new RubyDate(2021, 11, 22).toString()).toEqual('2021-11-22')
+})
+
 test('year', () => {
   const date = new RubyDate(2021, 5, 20)
   expect(date.year()).toEqual(2021)
--- src/ruby-date.ts
+++ src/ruby-date.ts
@@ -4,7 +4,12 @@ export default class RubyDate{
   private date: Date
 
   constructor(year: number, month: number, day: number){
-    this.date = new Date(year, month - 1, day)
+    const date = new Date(year, month - 1, day, 0, 0, 0, 0)
+    // Year is auto converted from 0 ~ 99 to 1900 ~ 1999
+    if(date.getFullYear() != year){
+      date.setFullYear(year)
+    }
+    this.date = date
   }
 
   day(): number{
@@ -17,6 +22,13 @@ export default class RubyDate{
 
   toDate(): Date{
     return new Date(this.date)
+  }
+
+  toString(){
+    const year = `${this.year()}`.padStart(4, '0')
+    const month = `${this.month()}`.padStart(2, '0')
+    const day = `${this.day()}`.padStart(2, '0')
+    return `${year}-${month}-${day}`
   }
 
   year(): number{
