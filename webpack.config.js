27f043f3f0e2226bc883bc305ae7bde0fe3e9782
kmdtmyk <winback_banderas@yahoo.co.jp>
2021-04-22 18:32:47 +0900

    Enable minify build by default

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,41 @@
 const path =  require('path')
 const TerserPlugin = require('terser-webpack-plugin')
 
-module.exports = {
-  mode: process.env.NODE_ENV || 'development',
-  entry: './src/index.ts',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'ruby-date.js',
-    library: 'RubyDate',
-    libraryTarget: 'umd',
-    globalObject: 'this',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: {
-          loader: 'ts-loader',
+function generateConfig({minify}){
+  return {
+    mode: minify ? 'production' : 'development',
+    entry: './src/index.ts',
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: `ruby-date${minify ? '.min' : ''}.js`,
+      library: 'RubyDate',
+      libraryTarget: 'umd',
+      globalObject: 'this',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: {
+            loader: 'ts-loader',
+          },
         },
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts'],
-  },
-  devtool: 'source-map',
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          keep_classnames: true,
-        },
-      })
-    ],
-  },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts'],
+    },
+    devtool: 'source-map',
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            keep_classnames: true,
+          },
+        })
+      ],
+    },
+  }
 }
+
+module.exports = generateConfig({minify: true})
