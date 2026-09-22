a87a2d05444f7df5e625d7701115f5634920deab
kmdtmyk <winback_banderas@yahoo.co.jp>
2026-09-22 16:56:31 +0900

    Refactoring

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,7 +1,5 @@
-import {copyFile, writeFile} from 'node:fs/promises'
+import {writeFile} from 'node:fs/promises'
 
-await copyFile('dist/cjs/ruby-date.js', 'dist/ruby-date.cjs')
-await copyFile('dist/cjs/ruby-date.js.map', 'dist/ruby-date.cjs.map')
 await writeFile('dist/cjs/package.json', '{"type":"commonjs"}\n')
 
 await writeFile(
